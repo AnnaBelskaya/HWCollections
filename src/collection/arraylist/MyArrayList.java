29f885d74c01a6d66eff0f73c57f2f6f2bbdcbd6
AnnaBelskaya <hannah.belskaya@gmail.com>
2017-09-08 09:25:23 +0300

    class MyArrayList changed

--- src/collection/arraylist/MyArrayList.java
+++ src/collection/arraylist/MyArrayList.java
@@ -1,91 +1,52 @@
 package collection.arraylist;
 
-import java.util.Arrays;
+import collection.MyCollection;
+import collection.linkedlist.Node;
 
-public class MyArrayList<T> {
-    private Object[] elements;
-    private int size;
-    private int capacity = 10;
+public class MyArrayList<T> extends MyCollection {
 
     public MyArrayList() {
-        elements = new Object[capacity];
-        size = 0;
+        super();
     }
 
     public MyArrayList(MyArrayList list) {
-        elements = list.getElements();
-        size = list.getSize();
-    }
-
-    public void add(T value){
-        if (elements == null) {
-            elements = new Object[capacity];
-            elements[size++] = value;
-        } else {
-            ensureCapacity();
-            elements[size++] = value;
-        }
-    }
-
-    private void ensureCapacity(){
-        if (size+1 > capacity){
-            capacity = (capacity*3)/2 + 1;
-            elements = Arrays.copyOf(elements, capacity);
-        }
-    }
-
-    public void clear(){
-        elements = null;
-        size = 0;
-        capacity = 10;
+        first = list.first;
+        size = list.size;
     }
 
     public Object get(int index){
-        return elements[index];
+        Node<T> focusNode = first;
+        while (--index >= 0){
+            focusNode = focusNode.next;
+        }
+        return focusNode.value;
     }
 
     public int indexOf(T value){
-        for (int i = 0; i < size; i++)
-            if (elements[i].equals(value))
+        Node<T> node = first;
+        int i = 0;
+        while (node!=null){
+            if(node.value.equals(value))
                 return i;
+            node = node.next;
+            i++;
+        }
         return -1;
     }
-
 
     public int indexOf(T value, int index){
-        for (int i = index; i < size; i++)
-            if (elements[i].equals(value))
+        Node<T> node = first;
+        int i = 0;
+        while (node!=null){
+            if(node.value.equals(value) && i >= index)
                 return i;
+            node = node.next;
+            i++;
+        }
         return -1;
-    }
-
-    public void remove(int index){
-        for (int i = index; i < size-1; i++)
-            elements[i] = elements[i+1];
-        elements = Arrays.copyOf(elements, --size);
     }
 
     public int size(){
-        return size;
-    }
-
-    @Override
-    public String toString() {
-        String out = "[";
-        for(int i = 0; i < size; i++){
-            if (i < size - 1)
-                out+=elements[i] + ", ";
-            else
-                out+=elements[i]+"]";
-        }
-        return out;
-    }
-
-    private Object[] getElements() {
-        return elements;
-    }
-
-    private int getSize() {
         return size;
     }
 }
